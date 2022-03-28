ee6a93c1846c07960bc6cfb3f1dc835a5ad19f9e
장소명 <somyeong3@naver.com>
2022-03-28 14:37:32 +0900

    [env] 환경설정 파일 추가

---
+++ backend/perfumee/src/main/java/com/ssafy/perfumee/PerfumeeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.perfumee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfumeeApplication.class, args);
+	}
+
+}
---
+++ backend/perfumee/src/main/java/com/ssafy/perfumee/model/entity/perfume/Contain.java
@@ -0,0 +1,34 @@
+package com.ssafy.perfumee.model.entity.perfume;
+
+import static javax.persistence.FetchType.LAZY;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import org.hibernate.type.descriptor.sql.TinyIntTypeDescriptor;
+
+@Entity
+public class Contain {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "CONTAIN_NO")
+  private Integer no;
+
+  @ManyToOne(fetch = LAZY)
+  @JoinColumn(name = "PERFUME_NO", nullable = false)
+  private Perfume perfume;
+
+  @ManyToOne(fetch = LAZY)
+  @JoinColumn(name = "NOTE_NO", nullable = false)
+  private Note note;
+
+  @Column(nullable = false)
+  private Boolean type;
+
+}
