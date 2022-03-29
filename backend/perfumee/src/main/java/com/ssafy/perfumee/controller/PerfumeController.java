6632bc39a53b23aba75cd2005ebda3b660a538fb
장소명 <somyeong3@naver.com>
2022-03-29 10:42:50 +0900

    [env] 폴더 구조 변경

---
+++ backend/perfumee/src/main/java/com/ssafy/perfumee/controller/PerfumeController.java
@@ -0,0 +1,12 @@
+package com.ssafy.perfumee.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/perfume")
+@RequiredArgsConstructor
+public class PerfumeController {
+
+}
