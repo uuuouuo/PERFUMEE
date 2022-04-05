e5fdcdf7cf2f1a764aa9e02e2e72b7c07e2c63f4
김일환 <ikhwan62@naver.com>
2022-04-05 23:55:20 +0900

    [fix] initDB 주석처리
    Former-commit-id: fb7e303f413584965bbbcdb3a2f716c5667105c0

--- backend/perfumee/src/test/java/com/ssafy/perfumee/PerfumeeApplicationTests.java
+++ backend/perfumee/src/test/java/com/ssafy/perfumee/PerfumeeApplicationTests.java
@@ -3,7 +3,7 @@ package com.ssafy.perfumee;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PerfumeeApplicationTests {
 
 	@Test
