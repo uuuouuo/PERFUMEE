180f30e0a0de629946619037689b89f417e50f8d
김일환 <ikhwan62@naver.com>
2022-04-05 23:35:43 +0900

    [refactor] file remove
    Former-commit-id: 9dd57f176aba10ff5b215bd203ce613a6128d49d

--- backend/recomm/backend/perfumee/src/main/java/com/ssafy/perfumee/config/jwt/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.perfumee.config.jwt;
-
-public interface JwtProperties {
-  String SECRET = "QUOKKACOLA";
-  int EXPIRATION_TIME = 1000 * 60 * 10;
-  String TOKEN_PREFIX = "Bearer ";
-  String HEADER_STRING = "JWT";
-}
