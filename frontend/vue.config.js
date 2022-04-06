5f8c8780ac305d67ba6936de675773542a79f077
LDH <tjrrbcl@naver.com>
2022-04-06 15:40:06 +0900

    [fix] error수정
    Former-commit-id: 80a6d6c2d7ece62061b065968ff935a84adc4ad8

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   publicPath: "./",
-  devServer: {
-    disableHostCheck: true,
-  },
   transpileDependencies: ["vuetify"],
 });
